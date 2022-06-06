8f0142c15cb808a07971869351e6dca655a58f1b
Nazril Afandi <95149134+NzrlAfndi@users.noreply.github.com>
2022-06-06 20:17:38 +0700

    Otomatis menyesuaikan session

--- connect/index.js
+++ connect/index.js
@@ -16,7 +16,7 @@ const FileType = require('file-type')
 const PhoneNumber = require('awesome-phonenumber')
 const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('../lib/exif')
 const { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('../lib/myfunc')
-const { state, saveState } = useSingleFileAuthState(`./sesi.json`)
+const { state, saveState } = useSingleFileAuthState(`./${global.sessionName}.json`)
 const store = makeInMemoryStore({ logger: pino().child({ level: 'fatal', stream: 'store' }) })
 global.api = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
 
