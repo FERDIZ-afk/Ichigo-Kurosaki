60f4a13f6f78cfa01e48d783a880f88c129caa89
Nazril Afandi <95149134+NzrlAfndi@users.noreply.github.com>
2022-12-27 12:04:34 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,8 +9,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Ubah Disini
-global.ownerNumber = ['6285609233482','6287877173955','0']
-global.ownerName = 'Fandyyy X All Creator'
+global.ownerNumber = ['6281540022632','6287877173955','0']
+global.ownerName = 'Whatsapp Developer'
 global.packname = 'Ichigo Kurosaki'
 global.author = 'Whatsapp Bot 2022'
 global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
